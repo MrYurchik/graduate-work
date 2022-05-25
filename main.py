08d3715140db8befa5e7bf5e88094c43b4ffdb29
MrYurchik <yur4ik322@gmail.com>
2022-05-25 18:34:23 +0300

    Test geolocation Accuracy

--- main.py
+++ main.py
@@ -98,13 +98,13 @@ def build_desc():
             icon=folium.Icon(color=random.choice(color_list), icon="info-sign"),
         ).add_to(m)
 
-def main():
-    leson_numb = time_parse()
-    schedule_for_today = get_schedule()
-    schedule_for_now = online_schedule(schedule_for_today, leson_numb)
-    build_geo = get_geo(schedule_for_now)
-    build_desc()
-    gen_map(build_geo)
+
+leson_numb = time_parse()
+schedule_for_today = get_schedule()
+schedule_for_now = online_schedule(schedule_for_today, leson_numb)
+build_geo = get_geo(schedule_for_now)
+build_desc()
+gen_map(build_geo)
 
 @app.route('/')
 def base():
@@ -116,5 +116,4 @@ def base():
 
 
 if __name__ == '__main__':
-    main()
     app.run(debug=True)
